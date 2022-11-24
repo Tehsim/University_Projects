7bfcd4df8b4cae7d6315569e24f234df7616c495
MSA Evan <msaiful.alam@petronas.com>
2022-11-24 21:40:50 +0600

    'ToDoList'

--- software_projects/SD 2 Project/ToDoList/src/ToDoList.java
+++ software_projects/SD 2 Project/ToDoList/src/ToDoList.java
@@ -1,36 +1,59 @@
 
 
 
-package to.pkgdo.list;
-
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
+// parent class
+class Intro {
+
+    // field and method of the parent class
+    public void Print1() {
+        System.out.println("\n====================== Software Development Project =======================\n");
+        System.out.println("\n                                TO Do List                \n");
+        System.out.println("\n===========================================================================");
+        System.out.println(" ");
+
+
+    }
+}
+
+//  subclass "Derived" inherit from parent class "Intro"
+class Derived extends Intro {
+
+    // new method in subclass
+    public void Print2() {
+        System.out.println(" Made By: \n");
+        System.out.println(" Team Leader: Tehsim Fariha \n");
+        System.out.println(" Team Members: Sadiya Afrin Priya,Nujhat Tabassum\n");
+        System.out.println(" Instructor: Mrs. Atia Binti Aziz, Adjunct Lecturer, Dept. of CSE, IIUC. \n");
+        System.out.println(" Institution: International Islamic University Chittagong \n");
+        System.out.println("\n---------------------------------------------------------------------------\n---------------------------------------------------------------------------\n");
+    }
+}
+
+
+
 public class ToDoList {
 
-    
-    
-    static String fileName = "D:\\data\\todolist.txt"; 
+
+    static String fileName =  "D:\\cp\\University_Projects\\software_projects\\SD 2 Project\\To Do List\\To Do List\\todolist.txt";
+
+
+
     public static void main(String[] args)throws IOException {
-        
-        
-        
-       // Scanner inpu = new Scanner(System.in);
-        System.out.println("\n\n ======Software Development Project=======\n");    
-        System.out.println("\n              TO Do List  \n               Project      \n ");    
-        System.out.println("\n==========================================");    
-        System.out.println(" ");    
-        System.out.println(" Made By: Tehsim Fariha,Sadiya Afrin Priya,Nujhat Tabassum\n");
-        System.out.println(" Instructor: Mrs. Atia Binti Aziz \n");
-        System.out.println(" Institution: International Islamic University Chittagong \n");
-        System.out.println("\n----------------------------------------------\n----------------------------------------------\n");         
-        
-        
-        
-        
-       // TODO code application logic here
-       
+
+
+        Derived d = new Derived();   // create an object of the subclass
+        d.Print1(); // print Base Class Method
+        d.Print2(); // print Derived Class Method
+
+
+
+        // TODO code application logic here
+
         int menuItem = -1;
         while(menuItem !=0){
             menuItem = menu();
@@ -43,38 +66,42 @@ public class ToDoList {
                     break;
                 case 3:
                     removeItem();
-                    break; 
-                case 0:  
-                    break; 
+                    break;
+                case 0:
+                    break;
                 default:
-                    System.out.println("Don't recognize input.");    
-                    
+                    System.out.println("Don't recognize input.");
+
             }
         }
-       
-  
+
+
     }
-    
+
+
+
+
     static int menu(){
         int choice;
         Scanner input = new Scanner(System.in);
-        System.out.println("\nMain Menu\n");    
-        System.out.println("0. Exit the program ");    
-        System.out.println("1. Dispaly to-do-list");    
-        System.out.println("2. Add item to to-do-list");    
-        System.out.println("3. Remove item from to-do list");    
-        System.out.println();                   
-        System.out.println("Enter a choice : ");                     
+        System.out.println("\nMain Menu\n");
+        System.out.println("0. Exit the program ");
+        System.out.println("1. Display to-do-list");
+        System.out.println("2. Add item to to-do-list");
+        System.out.println("3. Remove item from to-do list");
+        System.out.println();
+        System.out.println("Enter a choice : ");
         choice = input.nextInt();
         return choice;
-          
-    
-    
+
+
+
     }
-    
-    
+
+
 
     static void showList() {
+        System.out.println("\n Displaying ...... \n");
         System.out.println("\nTo-do List\n");
         try {
             Scanner inFile = new Scanner(new FileReader(fileName));
@@ -90,11 +117,11 @@ public class ToDoList {
             inFile.close();
         }
         catch (IOException ioe) {
-            System.out.println("Can't acess file.");
+            System.out.println("Can't access file.");
         }
     }
-    
-    
+
+
     static  void  addItem(){
         System.out.println("\nAdd Item\n");
         try {
@@ -106,13 +133,13 @@ public class ToDoList {
             outFile.close();
         }
         catch (IOException ioe) {
-            System.out.println("Can't acess file.");
+            System.out.println("Can't access file.");
         }
     }
-    
-     
+
+
     static  void   removeItem(){
-    
+        System.out.println("\nRemove Item\n");
         int choice;
         showList();
         Scanner input=new Scanner(System.in);
@@ -121,7 +148,7 @@ public class ToDoList {
         ArrayList<String> items= new ArrayList<String>();
         int number=1;
         try{
-            
+
             Scanner inFile=new Scanner(new FileReader(fileName));
             String item;
             while (inFile.hasNextLine()){
@@ -134,18 +161,18 @@ public class ToDoList {
             for (int i=0; i < items.size(); ++i)
                 outFile.println (items.get(i));
             outFile.close();
-            
+
         }
         catch(IOException ioe){
-                System.out.println("cannot access file.");
+            System.out.println("cannot access file.");
         }
-      
+
     }
 
-    
-   }
-    
-    
-    
-    
-    
+
+}
+
+
+
+
+
