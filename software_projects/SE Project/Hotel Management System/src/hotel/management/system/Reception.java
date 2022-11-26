129d8393d8afbc889ed9d687a943b0bff43c13c2
MSA Evan <msaiful.alam@petronas.com>
2022-11-26 23:55:38 +0600

    updatereception

--- software_projects/SE Project/Hotel Management System/src/hotel/management/system/Reception.java
+++ software_projects/SE Project/Hotel Management System/src/hotel/management/system/Reception.java
@@ -8,28 +8,28 @@ import java.awt.event.*;
 
 
 public class Reception extends JFrame implements ActionListener {
-    JButton newCustomer,rooms,department,allemployee,managerInfo,customers,searchRoom,update,pickup,logout,checkout,roomStatus;
+    JButton newCustomer,rooms,department,allemployee,managerInfo,customers,searchRoom,update,roomStatus,pickup,checkout,logout;;
     Reception(){
-
+        
         getContentPane(). setBackground(Color.WHITE);
         setLayout(null);
-
+        
         newCustomer = new JButton("New Customer Form");
         newCustomer.setBounds(10, 30, 200, 30);
         newCustomer.setBackground(Color.BLACK);
         newCustomer.setForeground(Color.WHITE);
         newCustomer.addActionListener(this);
         add(newCustomer);
-
-
+        
+        
         rooms = new JButton("Rooms");
         rooms.setBounds(10, 70, 200, 30);
         rooms.setBackground(Color.BLACK);
         rooms.setForeground(Color.WHITE);
         rooms.addActionListener(this);
         add(rooms);
-
-
+        
+        
         department = new JButton("Department");
         department.setBounds(10, 110, 200, 30);
         department.setBackground(Color.BLACK);
@@ -37,31 +37,31 @@ public class Reception extends JFrame implements ActionListener {
         department.addActionListener(this);
 
         add(department);
-
+        
         allemployee = new JButton("All Employees");
         allemployee.setBounds(10, 150, 200, 30);
         allemployee.setBackground(Color.BLACK);
         allemployee.setForeground(Color.WHITE);
         allemployee.addActionListener(this);
         add(allemployee);
-
+        
         customers = new JButton("Customer Info");
         customers.setBounds(10, 190, 200, 30);
         customers.setBackground(Color.BLACK);
         customers.setForeground(Color.WHITE);
         customers.addActionListener(this);
         add(customers);
-
-
+        
+        
         managerInfo = new JButton("Manager Info");
         managerInfo.setBounds(10, 230, 200, 30);
         managerInfo.setBackground(Color.BLACK);
         managerInfo.setForeground(Color.WHITE);
         managerInfo.addActionListener(this);
         add(managerInfo);
-
-
-         checkout = new JButton("Checkout");
+        
+        
+        checkout = new JButton("Checkout");
         checkout.setBounds(10, 270, 200, 30);
         checkout.setBackground(Color.BLACK);
         checkout.setForeground(Color.WHITE);
@@ -88,96 +88,80 @@ public class Reception extends JFrame implements ActionListener {
         pickup.setBackground(Color.BLACK);
         pickup.setForeground(Color.WHITE);
         pickup.addActionListener(this);
-        add(pickup);
-
+        add(pickup);;
+        
         searchRoom = new JButton("Search Room");
         searchRoom.setBounds(10, 430, 200, 30);
         searchRoom.setBackground(Color.BLACK);
         searchRoom.setForeground(Color.WHITE);
         searchRoom.addActionListener(this);
         add(searchRoom);
-
-
+        
+        
         logout = new JButton("Logout");
-        logout.setBounds(10, 310, 200, 30);
+        logout.setBounds(10, 470, 200, 30);
         logout.setBackground(Color.BLACK);
         logout.setForeground(Color.WHITE);
+        logout.addActionListener(this);
         add(logout);
-
-
-
+        
+        
+        
         ImageIcon il = new ImageIcon(ClassLoader.getSystemResource("icons/fourth.jpg"));
         JLabel image = new JLabel(il);
         image.setBounds(250, 30, 500, 470);
         add(image);
 
-
+        
         setBounds(350, 200, 800, 570);
         setVisible(true);
     }
     public void actionPerformed(ActionEvent ae)
-    {
+    { 
         if(ae.getSource()==newCustomer)
         {
             setVisible(false);
             new AddCustomer();
-
-        }
-        else if (ae.getSource()== rooms)
-        {
+            
+        } else if (ae.getSource()== rooms) {
             setVisible(false);
             new Room();
-
-        }
-        else if (ae.getSource()== department)
-        {
+       
+        } else if (ae.getSource()== department) {
             setVisible(false);
             new Department();
-        }
-        else if (ae.getSource()== allemployee)
-        {
+        }else if (ae.getSource()== allemployee) {
             setVisible(false);
             new EmployeeInfo();
-        }
-        else if (ae.getSource()== managerInfo)
-        {
+        }else if (ae.getSource()== managerInfo) {
             setVisible(false);
             new ManagerInfo();
-        }
-        else if (ae.getSource()== customers)
-        {
+        }else if (ae.getSource()== customers) {
             setVisible(false);
             new CustomerInfo();
-        }
-        else if (ae.getSource()== searchRoom)
-        {
+        }else if (ae.getSource()== searchRoom) {
             setVisible(false);
             new SearchRoom();
-        }
-        else if(ae.getSource()==update)
-        {
+        }else if (ae.getSource()== update) {
             setVisible(false);
             new UpdateCheck();
-        }
-        else if (ae.getSource()==pickup)
-        {
-            setVisible(false);
-            new Pickup();
-        }
-        else if (ae.getSource()==roomStatus)
-        {
+        }else if (ae.getSource()== roomStatus) {
             setVisible(false);
             new UpdateRoom();
-        }
-        else if (ae.getSource()== checkout)
-        {
+        }else if (ae.getSource()== pickup) {
+            setVisible(false);
+            new Pickup();
+        }else if (ae.getSource()== checkout) {
             setVisible(false);
             new Checkout();
+        }else if (ae.getSource()== logout) {
+            setVisible(false);
+            System.exit(0);
         }
-
+    
     }
     public static void main(String[] args) {
-
+        
         new Reception();
     }
 }
